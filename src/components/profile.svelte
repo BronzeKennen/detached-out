52c4abadeaa4029a57384e0fd6736de7f3048da8
BronzeKennen <sotoskand3@gmail.com>
2024-07-16 15:26:12 +0300

    yippie

---
+++ src/components/profile.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let name;
+    let count = 0;
+    function increment() {
+        count += 1
+    }
+</script>
+<style>
+    button {
+        color:black;
+        background-color: aquamarine;
+    }
+</style>
+<button on:click={increment}>{name.toUpperCase()} YIPPIE {count}</button>

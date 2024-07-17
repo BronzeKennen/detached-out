285193135a0ca43acd0276702eaa078732ccc80c
Christos Papadopoulos <chr.pap02@gmail.com>
2024-07-17 19:46:19 +0300

    den kserw ti ginetai haha

--- src/components/signin.svelte
+++ src/components/signin.svelte
@@ -3,28 +3,70 @@
 </script>
 
 <style>
+    .container {
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: start; /* align items at the start */
+        background-color: rgb(255, 255, 255);
+    }
+
     .signIn {
-        height: 400px;
-        margin-top: 20px;
-        margin-left: auto;
-        margin-right: auto;
-        width: 400px;
+        display: block;
+        height: 50%;
+        width: 25%;
+        margin: 10% auto;
         background-color: rgb(225, 225, 225);
     }
 
     .signIn b {
-        margin-left: auto;
-        margin-right: auto;
-        width: auto;
+        display: block; 
+        background-color: rgb(225, 225, 225);
+        text-align: center;
+        margin: 10% auto;
     }
 
     .signIn b img {
-        height: 40px; /* Adjust the height as needed */
-        padding: 5px; /* Adjust the padding as needed */
+        height: 100px; 
+        display: block;
+        margin: 0 auto;
+    }
+    
+    .signIn .textBoxes {
+        display: block; 
+        background-color: black;
+        margin: auto auto 30% auto;
+        height: 20%;
+        padding: 5%; 
+        color: white; 
+        width: 80%; 
+    }
+
+    .signIn .logInButton {
+        display: block;
+        text-align: center;
+        width: 30%; 
+        margin-top: -22% 75%;
+        padding: 1.5%;
+    }
+
+    input {
+        text-align: center;
+        margin: 2% auto 2% auto; 
+        width: 100%; 
+        padding: 1.5%; 
+        display: flex;
+        box-sizing: border-box; 
     }
 </style>
 
-
-<div class='signIn'>
-    <b><img src={Logo} alt="Logo" /></b>
+<div class="container">
+    <div class='signIn'>
+        <b><img src={Logo} alt="Logo" /></b>
+        <div class ='textBoxes'>
+            <input type='email' placeholder="Email" />
+            <input type='password' placeholder="Password" />
+        </div>
+        <button class='logInButton'>Press</button>
+    </div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,6 @@
     import Navbar from '../components/navbar.svelte';
     import SignIn from '../components/signin.svelte';
 </script>
-
-<Navbar />
+<!-- 
+<Navbar /> -->
 <SignIn />
-<h1>Future Login/Signup</h1>
-<p>Navbar Will be removed</p>

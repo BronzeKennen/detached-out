62e4756691260300528ae7a009c9d3ffbc1ecb03
BronzeKennen <sotoskand3@gmail.com>
2024-08-28 15:55:41 +0300

    fixed?

--- src/components/profileIcon.svelte
+++ src/components/profileIcon.svelte
@@ -1,4 +1,5 @@
 
+
 <script>
     export let user;
     export let edu;
@@ -6,8 +7,8 @@
     export let pfp;
 
     let resp;
+    
 </script>
-
     <span class="stats">
         {#if pfp}
             <span id="pfp" style={`background-image: url(${pfp})`}></span>
@@ -15,61 +16,38 @@
             <span id="pfp" ></span>
         {/if}
         <div class="substats">
-            <h3 class="username"><a href="/pages/profile/{id}">{user}</a></h3>
+            <h3 class="username">{user}</h3>
             {#if edu}
-                <p class="subtitle">{edu.university_name} • {edu.major}</p>
+                <p class="subtitle">{edu.university_name} * {edu.major}</p>
             {/if}
         </div>
     </span>
 <style>
-    a {
-        text-decoration: none;
-
-    }
-    a:visited {
-        color: inherit;
-    }
-    
     .stats {
         display:flex;
         flex-direction: row;
         align-items: center;
-        width: 275px;
+        min-width:100%;
         justify-content: flex-start;
     }
     .substats {
         display:flex;
         flex-direction: column;
-        flex-grow: 1;
     }
     .subtitle {
-        max-width: 200px;
-        font-size: small;
-        overflow: hidden;
-        white-space: nowrap;
-        text-overflow: ellipsis;
+        font-size:85%;
     }
     #pfp {
         margin:.5rem;
         min-width:50px;
         min-height:50px;
-        width: 50px;
-        height: 50px;
         background-color:cyan;
         border-radius:50%;
         background-size: cover; /* Ensures the image covers the element */
         background-position: center; /* Centers the image within the element */
         background-repeat: no-repeat;
     }
-    @media (max-width:950px) {
-        .stats {
-            width: 200px;
-        }
-
-        .subtitle {
-            font-size: x-small;
-            max-width: 125px;
-        }
+    @media (max-width:870px) {
     }
 
 
